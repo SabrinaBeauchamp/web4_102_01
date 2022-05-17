a8a7f34541dcb50a5ffec48975c3e210f5dba150
WilliamCharbonneau <1973389@cstj.qc.ca>
2022-05-17 10:16:13 -0400

    amélioration des cartes forfaits et ajout de css

--- resources/views/forfaits/index.blade.php
+++ resources/views/forfaits/index.blade.php
@@ -1,37 +1,45 @@
 @extends('mesLayouts.layout')
 
 @section('titre')
-    Tout les forfaits
+Tout les forfaits
 @endsection
 
 @section('contenu')
 <div>
-    <div class="conteneurForfait">
-        <div class="conteneurImgForfait">
-            <img src="" alt="" class="imgForfait">
-            <h1>Forfait escapade gourmande 1</h1>
+    <div class="conteneurForfaits">
+        <div class="conteneurForfait">
+            <div class="conteneurImgForfait">
+                <img src="" alt="" class="imgForfait">
+                <h1>Forfait escapade gourmande</h1>
+            </div>
+            <div class="conteneurInfoForfait">
+                <h2>Catch phrase!</h2>
+                <p>On aime ça faire pousser des plantes icite.</p>
+                <a href="" class="boutonForfait">Découvrez ce forfait</a>
+            </div>
         </div>
-        <div class="conteneurInfoForfait">
-            <h2>Catch phrase!</h2>
-            <p>On aime ça faire pousser des plantes icite.</p>
-            <a href="" class="boutonForfait">Découvrez ce forfait</a>
-        </div>
-        <div class="conteneurImgForfait">
-            <img src="" alt="" class="imageForfait">
-            <h1>Forfait escapade gourmande 2</h1>
-        </div>
-        <div class="conteneurInfoForfait">
-            <h2>Catch phrase!</h2>
-            <p>On aime ça faire pousser des plantes icite.</p>
-            <a href="" class="boutonForfait">Découvrez ce forfait</a>
+        <div class="conteneurForfait">
+            <div class="conteneurImgForfait">
+                <img src="" alt="" class="imgForfait">
+                <h1>Forfait découverte</h1>
+            </div>
+            <div class="conteneurInfoForfait">
+                <h2>Catch phrase!</h2>
+                <p>On aime ça faire pousser des plantes icite.</p>
+                <a href="" class="boutonForfait">Découvrez ce forfait</a>
+            </div>
         </div>
     </div>
+    <div class="conteneurCirclePage">
+        <input type="radio" class="radio1">
+        <input type="radio" class="radio2">
+    </div>
 </div>
-    @if($forfaits->count() > 0)
-            @include('forfaits.list')
-        @else
-            <p>la liste est vide</p>
-    @endif
-    <x-champ-lien href="{{route('forfaits.create')}}" titre="Créer un nouveau forfait"></x-champ-lien>
-   
+@if($forfaits->count() > 0)
+@include('forfaits.list')
+@else
+<p>la liste est vide</p>
+@endif
+<x-champ-lien href="{{route('forfaits.create')}}" titre="Créer un nouveau forfait"></x-champ-lien>
+
 @endsection
