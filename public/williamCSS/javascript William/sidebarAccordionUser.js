cf2b4fa4cca984025a3e4331335ab7a26953047c
William <1973389@cstj.qc.ca>
2022-05-24 14:20:18 -0400

    ajout dashboard basic +js pour interact

---
+++ public/williamCSS/javascript William/sidebarAccordionUser.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    //document.getElementById("sidebarFav").style.height = "100px";
+    document.getElementById("sidebarFav").style.display = "block";
+    //ajouter classe  pour ensuite desactiver
+    document.getElementById("arrowFav").style.transform = "rotate(270deg)";
+  }
