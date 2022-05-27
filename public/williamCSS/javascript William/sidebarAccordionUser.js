6cf2ac0954331db7f1704e43ed9bbad85af33b2b
WilliamCharbonneau <1973389@cstj.qc.ca>
2022-05-26 21:21:10 -0400

    Menu Hamburger animated + js

--- public/williamCSS/javascript William/sidebarAccordionUser.js
+++ public/williamCSS/javascript William/sidebarAccordionUser.js
@@ -13,3 +13,30 @@
       document.getElementById("arrowFav").style.marginleft = "none";
     }
   })
+
+  let dashMenu =document.querySelector(".conteneurSidebar")
+  let btnOpen = document.querySelector(".boutonOpen");
+  let btnClose = document.querySelector(".boutonClose");
+
+  // btn.addEventListener('click', () => {
+  //   if(dashMenu.style.width === "20%") {
+  //     dashMenu.style.width ='0%';
+  //     dashMenu.style.display ='none';
+      
+  //   }
+  //   else{
+  //     dashMenu.style.width='20%'; dashMenu.style.display ='flex';
+  //   }
+  // })
+
+
+  btnOpen.addEventListener('click', () => {
+    dashMenu.style.width = '20%';
+    btnOpen.style.visibility ='hidden';
+  })
+
+  btnClose.addEventListener('click', () => {
+    dashMenu.style.width = '0px';
+    btnOpen.style.visibility ='visible';
+
+  })
