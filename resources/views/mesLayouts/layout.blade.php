8729dc81a027fcc09f5c1e2ca6e4d13f68ef9962
Sabrina Beauchamp <1805429@cstj.qc.ca>
2022-05-31 14:32:12 -0400

    Revert "Merge branch 'felix' into favories"
    This reverts commit 79894536d250fef988e5a0ccb103780f4813184c, reversing
    changes made to 814f28627f608fe07491a4d161492aa6eaa6a6b9.

--- resources/views/mesLayouts/layout.blade.php
+++ resources/views/mesLayouts/layout.blade.php
@@ -9,7 +9,6 @@
     <!-- <link rel="stylesheet" href="{{asset('')}}"> -->
     <script src="{{asset('js/Favori.js')}}"></script>
     <script src="{{asset('js/Populaire.js')}}"></script>
-    <link rel="stylesheet" href="{{asset('CSS/style.css')}}">
     <title>@yield("titre") - Les Forfaits</title>
 </head>
 <body>
@@ -19,18 +18,11 @@
                 <h1 class="titre">@yield("titre")</h1>
             </a>
         </header>
+        <footer></footer>
         <div class="main">
             @yield("contenu")
             
         </div>
-        <footer>
-            <div class="infolettre">
-                <h3>
-                    Ceci est une infolettre
-                </h3>
-            </div>
-            <p>hello world</p>
-        </footer>
     </div>
 </body>
 </html>
