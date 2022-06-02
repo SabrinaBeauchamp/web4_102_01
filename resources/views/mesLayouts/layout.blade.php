420fdd8a7823293b6670a226547657796dd6e669
Felix <1852184@cstj.qc.ca>
2022-06-02 19:31:53 -0400

    reglage du bug dans layout

--- resources/views/mesLayouts/layout.blade.php
+++ resources/views/mesLayouts/layout.blade.php
@@ -28,42 +28,7 @@
                 </div>
                 <!-- PANNEAUX -->
                 <div class="container-panneau">
-                    <div class="panneau0 isPanneau panneau-close">
-                        <ul class="menu0">
-                            <li>
-                                <a class="btn1" href="{{route('categoriesRegion.index')}}">MRC</a>
-                            </li>
-                            <li>
-                                <a class="btn1" href="{{route('forfaits.categories.index')}}">Catégories</a>
-                            </li>
-                            <li>
-                                <a class="btn1" href="{{route('evenements.index')}}">Évènements</a>
-                            </li>
-                            @if(Auth::user())
-                                <li><a href="{{route('users.gestionaires.index')}}">Compte</a></li>
-                                <li>
-                                    <form action="{{route('logout')}}" method="POST">
-                                        @csrf
-                                        <button class="btn1" type="submit">Deconnexion</button>
-                                    </form>
-                                </li>
-                            @else
-                                <li>
-                                    <a class="btn1" href="{{route('login')}}">Me connecter</a>
-                                </li>
-                            @endif
-                        </ul>
-                    </div>
-                    <div class="panneau isPanneau panneau-close">
-                        <h2>Groupes</h2>
-                        <ul class="menu1">
-                            @foreach($groupes as $groupe)
-                                <li>
-                                    <a href="{{route('groupes.show', ['groupe'=>$groupe])}}" class="btn1">{{$groupe['nom']}}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                    @yield('panneaux0')
                     @yield('panneaux')
                 </div>
             </nav>
