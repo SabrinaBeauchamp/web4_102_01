dbd12a674e55cb08d7545ad0e0317075b4883296
Felix <1852184@cstj.qc.ca>
2022-06-02 19:12:34 -0400

    correction de laffichage du titre des mrc

--- public/felixJs/Carte.js
+++ public/felixJs/Carte.js
@@ -12,18 +12,18 @@ class Carte {
         var noms, cartes;
         noms = document.querySelector(".liste-carte").childNodes;
         cartes = document.querySelector(".container-background-carte").childNodes;
-        for (let i = 0; i < noms.length; i++) {
-            const carte = cartes[i];
-            noms[i].addEventListener("mouseover", e => {
-                carte.style.opacity = "1";
-                e.preventDefault();
-            });
-            noms[i].addEventListener("mouseout", e => {
-                carte.style.opacity = "0.1";
-                e.preventDefault();
-            });
-            
-
+        if(noms != null){
+            for (let i = 0; i < noms.length; i++) {
+                const carte = cartes[i];
+                noms[i].addEventListener("mouseover", e => {
+                    carte.style.opacity = "1";
+                    e.preventDefault();
+                });
+                noms[i].addEventListener("mouseout", e => {
+                    carte.style.opacity = "0.1";
+                    e.preventDefault();
+                });
+            }
         }
     }
 
