07212dbb24ef6f2663127ef4e690eae6a7c57a9d
Mathis Leblanc <1949383@cstj.qc.ca>
2022-06-02 12:42:35 -0400

    Correction css de index de categories et entreprises

--- resources/views/entreprises/index.blade.php
+++ resources/views/entreprises/index.blade.php
@@ -6,7 +6,13 @@
 
 @section('contenu')
 
-    @include('entreprises.liste')
+    @foreach($entreprises as $entreprise)
+    <li>
+        <a class="btn3 commodites" href="{{route('entreprises.show', $entreprise)}}">
+            {{$entreprise->nom}}
+        </a>
+    </li>
+    @endforeach
     @if(Auth::check() && Auth::user()->role === 'admin')
     <a class="boutonUniforme" href="{{route('entreprises.create')}}">Ajouter une entreprise</a>
     @endif
