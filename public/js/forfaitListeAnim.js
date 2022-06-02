f1b10d034a45241f10b1b336de75052777c0eae1
Sabrina Beauchamp <1805429@cstj.qc.ca>
2022-06-02 15:01:04 -0400

    correction du merge

--- public/js/forfaitListeAnim.js
+++ public/js/forfaitListeAnim.js
@@ -2,18 +2,20 @@ let fleche = document.querySelector(".fa-chevron-up");
 let menu = document.querySelector(".conteneurBtnCarte");
 let conteneurCarte = document.querySelector(".conteneurForfait")
 
-fleche.addEventListener('click', () => {
-    if(menu.style.height === "120px") {
-      menu.style.height ='0px';
-     fleche.style.transform = "rotate(180deg)";
-     conteneurCarte.style.height ='330px';
-      
-    }
-    else{
-      menu.style.height='120px';
-      fleche.style.transform = "none";
-      conteneurCarte.style.height ='450px';
-    }
-  })
+if(fleche) {
+  fleche.addEventListener('click', () => {
+      if(menu.style.height === "120px") {
+        menu.style.height ='0px';
+       fleche.style.transform = "rotate(180deg)";
+       conteneurCarte.style.height ='330px';
+        
+      }
+      else{
+        menu.style.height='120px';
+        fleche.style.transform = "none";
+        conteneurCarte.style.height ='450px';
+      }
+    })
+}
 
   
