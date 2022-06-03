f09adbde6252d2e3f94579ed159d607fdc2f43c3
Felix <1852184@cstj.qc.ca>
2022-06-03 14:45:55 -0400

    menu dans forfaits

--- resources/views/forfaits/create.blade.php
+++ resources/views/forfaits/create.blade.php
@@ -4,6 +4,48 @@
     Nouveau forfait
 @endsection
 
+@section('panneaux0')
+    <div class="panneau0 isPanneau panneau-close">
+        <ul class="menu0">
+            <li>
+                <a class="btn1" href="{{route('acceuil')}}">Acceuil</a>
+            </li><li>
+                <a class="btn1" href="{{route('categoriesRegion.index')}}">MRC</a>
+            </li>
+            <li>
+                <a class="btn1" href="{{route('forfaits.categories.index')}}">Catégories</a>
+            </li>
+            <li>
+                <a class="btn1" href="{{route('evenements.index')}}">Évènements</a>
+            </li>
+            @if(Auth::user())
+                <li><a href="{{route('users.gestionaires.index')}}">Compte</a></li>
+                <li>
+                    <form action="{{route('logout')}}" method="POST">
+                        @csrf
+                        <button type="submit">Deconnexion</button>
+                    </form>
+                </li>
+            @else
+                <li>
+                    <a href="{{route('login')}}">Me connecter</a>
+                </li>
+            @endif
+        </ul>
+    </div>
+    <div class="panneau isPanneau panneau-close">
+        <ul class="menu1">
+            <li>
+                <a href="{{route('groupes.show', ['groupe'=>$groupe])}}" class="btn1">Accueil</a>
+            </li>
+        </ul>
+    </div>
+@endsection
+
+@section('panneaux')
+<!-- index n'a pas de panneaux supplémentaires -->
+@endsection
+
 @section('contenu')
 <h2>Création d'un nouveau forfait</h2>
     <form action="{{route('forfaits.store')}}" method="post" enctype="multipart/form-data">
